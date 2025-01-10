a63b1134167dcf3ba0d01724f868bb5c4a77f4cd
dorian2023 <doriangonzalez2018@gmail.com>
2025-01-10 15:06:55 -0400

    ultimos cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,20 @@
-'use client'
+"use client";
 
 import AvatarLogo from "@/components/avatar-logo";
 import Introduction from "@/components/introduction";
 import TransitionPage from "@/components/transition-page";
-
 
 export default function Home() {
   return (
     <main>
       <TransitionPage />
       <div className="flex min-h-[100vh] h-full bg-no-repeat">
-   
+        <AvatarLogo
+          className={`flex justify-start md:justify-start  md:left-1 absolute w-11/12 md:absolute pt-32 pb-0`}
+        />
 
-      <AvatarLogo className={`flex justify-start md:justify-start  md:left-1 absolute w-full md:absolute pt-32 pb-0`} />
-        
-        <Introduction/>
-        
+        <Introduction />
       </div>
     </main>
   );
 }
-
-
-
-
